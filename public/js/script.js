c57b6a49b1f5b1b08397be097f0211a2c8d5b899
Artemis051 <aekatsi@gmail.com>
2024-01-28 21:34:36 -0500

    heroku

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,21 @@
 //JavaScript for the front end goes here
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const api_key = 'My_API_Key';
+// Middleware to parse request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Route for search/fetch
+app.post('/search', async (req, res) => {
+    const { query } = req.body;
+    const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${MY_API_Key}`);
+    const recipes = response.data.results;
+    res.render('results', { recipes });
+});
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
